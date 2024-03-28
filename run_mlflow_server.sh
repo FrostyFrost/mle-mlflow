97606f259f3836476c28ba5dbee9187602029cb0
Andrey <asmokhov@mail.ru>
2024-03-28 23:36:27 +0300

    mlflow: correct tracking_url

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -4,7 +4,7 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_BUCKET_NAME=$S3_BUCKET_NAME
 
 mlflow server \
-    --registry-store-uri   s3://$AWS_BUCKET_NAME  \
+    --registry-store-uri   postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME  \
     --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
     --default-artifact-root s3://$AWS_BUCKET_NAME \
     --no-serve-artifacts 
